c884e82827f465dc89dc4473884a240502f50d9c
中安佑一 <yuichi.nakayasu@gmail.com>
2017-09-30 01:58:40 +0900

    R.swift・ストーリーボード用の拡張

---
+++ Formations/Sources/Extensions/Rswift+Storyboard.swift
@@ -0,0 +1,33 @@
+// =============================================================================
+//  Formations
+//  Copyright 2017 yuichi.nakayasu All rights reserved.
+// =============================================================================
+import UIKit
+import Rswift
+
+extension StoryboardResourceWithInitialControllerType {
+    
+    func instantiate<T: UIViewController>(_ type: T.Type, setup: ((T)->Void)? = nil) -> T {
+        guard let vc = self.instantiateInitialViewController() as? T else {
+            fatalError("faild create view-controller from storyboard.")
+        }
+        setup?(vc)
+        return vc
+    }
+    
+    func instantiate<T: UIViewController>(withinNavigation type: T.Type, setup: ((T)->Void)? = nil) -> UINavigationController {
+        let vc = instantiate(type)
+        setup?(vc)
+        return UINavigationController(rootViewController: vc)
+    }
+}
+
+extension UIStoryboard {
+    
+    func instatiate<T: UIViewController>(_ type: T.Type, _ storyboardIdentifier: String) -> T {
+        guard let vc = self.instantiateViewController(withIdentifier: storyboardIdentifier) as? T else {
+            fatalError("faild create view-controller from storyboard.")
+        }
+        return vc
+    }
+}
