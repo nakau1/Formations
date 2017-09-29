593880630f3eccf962c27021e3933666d77dd4db
中安佑一 <yuichi.nakayasu@gmail.com>
2017-09-30 03:33:32 +0900

    テスト画面作成 fix https://github.com/nakau1/Formations/issues/9

---
+++ Formations/Sources/Tests/TestViewController.swift
@@ -0,0 +1,25 @@
+// =============================================================================
+//  Formations
+//  Copyright 2017 yuichi.nakayasu All rights reserved.
+// =============================================================================
+import UIKit
+import Rswift
+
+// MARK: - Controller Definition -
+class TestViewController: UIViewController {
+    
+    // MARK: ファクトリメソッド
+    class func create() -> UIViewController {
+        return R.storyboard.testViewController.instantiate(self) { vc in
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction private func didTapTest() {
+        print(1)
+    }
+}
