1c89f06cbcaeddd76dec1cd2c671d5c70bfded85
中安佑一 <yuichi.nakayasu@gmail.com>
2017-09-30 01:59:05 +0900

    ビューコントローラの基本実装

--- Formations/Sources/Modules/Team/Views/Edit/TeamEditViewController.swift
+++ Formations/Sources/Modules/Team/Views/Edit/TeamEditViewController.swift
@@ -8,11 +8,25 @@ import Rswift
 // MARK: - Controller Definition -
 class TeamEditViewController: UIViewController {
     
+    // MARK: ファクトリメソッド
+    class func create() -> UIViewController {
+        return R.storyboard.teamEditViewController.instantiate(self) { vc in
+            
+        }
+    }
+    
+    // MARK: ライフサイクル
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        prepare()
+    }
 }
 
 // MARK: - Private -
 private extension TeamEditViewController {
     
+    /// ビューの初期処理
     func prepare() {
         
     }
