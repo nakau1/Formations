c29d3c9a0014b18c1e85a42ddb79ff6ebf983f7a
中安佑一 <yuichi.nakayasu@gmail.com>
2017-09-30 01:47:09 +0900

    Rswiftをプロジェクトに導入 http://dev.classmethod.jp/smartphone/iphone/r-swift-introduction/

--- Formations/Sources/Modules/Team/Views/Edit/TeamEditViewController.swift
+++ Formations/Sources/Modules/Team/Views/Edit/TeamEditViewController.swift
@@ -3,6 +3,7 @@
 //  Copyright 2017 yuichi.nakayasu All rights reserved.
 // =============================================================================
 import UIKit
+import Rswift
 
 // MARK: - Controller Definition -
 class TeamEditViewController: UIViewController {
