95b70f153c4b096b71e5b0661ee3fd5d27dc4bae
中安佑一 <yuichi.nakayasu@gmail.com>
2017-10-03 23:03:25 +0900

    モデルに画像保存と削除を実装

--- Formations/Sources/Modules/Team/Models/TeamModel.swift
+++ Formations/Sources/Modules/Team/Models/TeamModel.swift
@@ -3,6 +3,7 @@
 //  Copyright 2017 yuichi.nakayasu All rights reserved.
 // =============================================================================
 import Foundation
+import CoreGraphics
 
 class TeamModel: RealmModel<Team>, IdentifierGeneratable {
 	
@@ -10,6 +11,40 @@ class TeamModel: RealmModel<Team>, IdentifierGeneratable {
         let ret = super.create()
         ret.id = generateIdentifier()
         return ret
+    }
+    
+    override func save(entities: [Entity]) {
+        super.save(entities: entities)
+        saveImages(entities: entities)
+    }
+    
+    override func update(_ condition: NSPredicate? = nil, updating: (Entity, Int) -> Void) {
+        super.update(condition, updating: updating)
+        saveImages(entities: select(condition).toArray())
+    }
+    
+    override func delete(_ condition: NSPredicate? = nil) {
+        super.delete(condition)
+        deleteImages(entities: select(condition).toArray())
+    }
+    
+    private func saveImages(entities: [Entity]) {
+        entities.forEach { entity in
+            let emblemImage = entity.loadEmblemImage().emblemImage?.adjusted(to: CGSize(400, 400), shouldExpand: true)
+            Image.teamEmblem(id: entity.id).save(emblemImage)
+            
+            let smallEmblemImage = entity.loadSmallEmblemImage().smallEmblemImage?.adjusted(to: CGSize(210, 210), shouldExpand: true)
+            Image.teamSmallEmblem(id: entity.id).save(smallEmblemImage)
+            
+            let teamImage = entity.loadTeamImage().teamImage?.adjusted(to: CGSize(828, 1472), shouldExpand: true)
+            Image.teamImage(id: entity.id).save(teamImage)
+        }
+    }
+    
+    private func deleteImages(entities: [Entity]) {
+        entities.forEach { entity in
+            Image.delete(category: .teams, id: entity.id)
+        }
     }
 }
 
