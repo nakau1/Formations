17fa5fc1721b6354efd8e3b3be6799664f811ea6
中安佑一 <yuichi.nakayasu@gmail.com>
2017-10-04 02:14:30 +0900

    テスト更新

--- Formations/Sources/Tests/TestViewController.swift
+++ Formations/Sources/Tests/TestViewController.swift
@@ -8,6 +8,8 @@ import Rswift
 // MARK: - Controller Definition -
 class TestViewController: UIViewController {
     
+    @IBOutlet private weak var board: PositionBoardView!
+    
     // MARK: ファクトリメソッド
     class func create() -> UIViewController {
         return R.storyboard.testViewController.instantiate(self)
@@ -15,10 +17,23 @@ class TestViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        board.pins = (0..<11).map { i -> PositionBoardPin in
+            let view = UIView(frame: CGRect(width: 44, height: 44))
+            view.layer.cornerRadius = 22
+            view.backgroundColor = .black
+            let p = (i.f * 40.f) / (11.f * 40.f)
+            return PositionBoardPin(view: view, percentage: CGPercentage(p, p))
+        }
+        board.reloadData()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
     }
     
     @IBAction private func didTapTest() {
-		
-		BackgroundView.notifyChangeImage(nil)
+		board.reloadData()
     }
 }
