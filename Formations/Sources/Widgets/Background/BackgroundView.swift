c42d6b759a9d38ed4994acfc180a97add3450b56
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2017-10-03 16:19:23 +0900

    背景画像ビューの実装

--- Formations/Sources/Widgets/Background/BackgroundView.swift
+++ Formations/Sources/Widgets/Background/BackgroundView.swift
@@ -5,14 +5,48 @@
 import UIKit
 import Rswift
 
+/*
+ BackgroundView の constraint の貼り方
+ 
+ 1. vertical, horizontal ともに center にする
+ 2. 各面の align を superview(safe-areaではなく) に 0 で設定する
+ 3. leading, trailing の priority を 750 にする
+ 4. aspect を　414:736 にする
+ */
+
 class BackgroundView: UIImageView {
+    
+    private static let NotificationImageKey = "image"
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.image = R.image.sampleEmblem1()!
+        if image == nil {
+            image = R.image.defaultBackground()
+        }
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(didReceiveDidChangeImage(notification:)),
+            name: .BackgroundViewDidChangeImage,
+            object: nil
+        )
+    }
+    
+    @objc func didReceiveDidChangeImage(notification: Notification) {
+        if let image = notification.userInfo?[BackgroundView.NotificationImageKey] as? UIImage {
+            self.image = image
+        }
+    }
+    
+    class func notifyChangeImage(_ image: UIImage?) {
+        let postingImage = image ?? R.image.defaultBackground()!
+        NotificationCenter.default.post(
+            name: .BackgroundViewDidChangeImage,
+            object: nil,
+            userInfo: [BackgroundView.NotificationImageKey : postingImage]
+        )
     }
 }
 
 extension Notification.Name {
-    
+    static let BackgroundViewDidChangeImage = Notification.Name("BackgroundViewDidChangeImage")
 }
