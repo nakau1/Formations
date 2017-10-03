bd45b4f4d57bbc7e10757c7274c9dc66bb49bd54
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2017-10-03 21:18:04 +0900

    Realmモデルに関するクラスの追加

---
+++ Formations/Sources/Models/Fixture.swift
@@ -0,0 +1,25 @@
+// =============================================================================
+//  Formations
+//  Copyright 2017 yuichi.nakayasu All rights reserved.
+// =============================================================================
+import Foundation
+import RealmSwift
+import Rswift
+
+class Fixture {
+    
+    static let shared = Fixture()
+    private init() {}
+    
+    func fix() {
+        
+    }
+    
+    func reset() {
+        
+    }
+}
+
+private extension Fixture {
+    
+}
---
+++ Formations/Sources/Models/RealmModel.swift
@@ -0,0 +1,68 @@
+// =============================================================================
+//  Formations
+//  Copyright 2017 yuichi.nakayasu All rights reserved.
+// =============================================================================
+import Foundation
+import RealmSwift
+
+class Realm {}
+
+class RealmModel<T: RealmSwift.Object> {
+    
+    typealias Entity = T
+    
+    class var realmPath: String {
+        return RealmSwift.Realm.Configuration.defaultConfiguration.fileURL?.absoluteString ?? ""
+    }
+    
+    func loadRealm() -> RealmSwift.Realm {
+        return try! RealmSwift.Realm()
+    }
+    
+    // MARK: - CRUD
+    
+    func create() -> Entity {
+        return Entity()
+    }
+    
+    func select(_ condition: NSPredicate? = nil) -> RealmSwift.Results<Entity> {
+        if let conditionPredicate = condition {
+            return loadRealm().objects(Entity.self).filter(conditionPredicate)
+        } else {
+            return loadRealm().objects(Entity.self)
+        }
+    }
+    
+    func count(_ condition: NSPredicate? = nil) -> Int {
+        return select(condition).count
+    }
+    
+    func save(entities: [Entity]) {
+        let realm = loadRealm()
+        try! realm.write {
+            realm.add(entities, update: true)
+        }
+    }
+    
+    func save(_ entity: Entity) {
+        save(entities: [entity])
+    }
+    
+    func update(_ condition: NSPredicate? = nil, updating: (Entity, Int) -> Void) {
+        let realm = loadRealm()
+        try! realm.write {
+            select(condition).enumerated().forEach { i, entity in
+                updating(entity, i)
+            }
+        }
+    }
+    
+    func delete(_ condition: NSPredicate? = nil) {
+        let realm = loadRealm()
+        try! realm.write {
+            realm.delete(select(condition))
+        }
+    }
+}
+
+
