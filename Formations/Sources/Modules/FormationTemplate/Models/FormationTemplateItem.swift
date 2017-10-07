a702cc7bd0aa0e158a45fddb2bdb849485224f8b
中安佑一 <yuichi.nakayasu@gmail.com>
2017-10-07 23:38:43 +0900

    フォーメーション編集画面用の画像素材

---
+++ Formations/Sources/Modules/FormationTemplate/Models/FormationTemplateItem.swift
@@ -0,0 +1,9 @@
+//
+//  FormationTemplateItem.swift
+//  Formations
+//
+//  Created by 中安佑一 on 2017/10/07.
+//  Copyright © 2017年 yuichi.nakayasu@gmail.com. All rights reserved.
+//
+
+import Foundation
