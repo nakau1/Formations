c7cb537de2f84434e2d31e62ad464c02a195c5f5
中安佑一 <yuichi.nakayasu@gmail.com>
2017-10-08 02:44:38 +0900

    フォーメーション雛形編集。フォーメーションの配置編集部分の実装

--- Formations/Sources/Modules/FormationTemplate/Views/Edit/FormationTemplateEditViewController.swift
+++ Formations/Sources/Modules/FormationTemplate/Views/Edit/FormationTemplateEditViewController.swift
@@ -9,25 +9,68 @@ import Rswift
 class FormationTemplateEditViewController: UIViewController {
     
     // MARK: ファクトリメソッド
-    class func create() -> UIViewController {
+    class func create(for template: FormationTemplate?) -> UIViewController {
         return R.storyboard.formationTemplateEditViewController.instantiate(self) { vc in
-            
+            if let template = template {
+                vc.template = template
+            } else {
+                let newTemplate = Realm.FormationTemplate.create()
+                vc.template = newTemplate
+                vc.isAdd = true
+            }
         }
     }
     
-    // MARK: ライフサイクル
+    @IBOutlet private weak var positionBoard: PositionBoardView!
+    
+    private var isAdd = false
+    private var template: FormationTemplate!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        prepare()
-    }
-}
-
-// MARK: - Private -
-private extension FormationTemplateEditViewController {
-    
-    /// ビューの初期処理
-    func prepare() {
         prepareNavigationBar()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        preparePositionBoardView()
+        positionBoard.reloadData()
+    }
+    
+    func preparePositionBoardView() {
+        positionBoard.pins = template.items.map { item -> PositionBoardPin in
+            let view = FormationTemplatePin.create(position: item.position)
+            return PositionBoardPin(view: view, percentage: item.percentage)
+        }.sorted { a, b in
+            return a.percentage < b.percentage
+        }
+        positionBoard.moved = { [unowned self] pin, i in
+            self.didMovedFormationItem(self.template.items[i], percentage: pin.percentage)
+        }
+    }
+    
+    @IBAction private func didTapCompleteButton() {
+        AlertDialog.showConfirmNewSave(
+            from: self,
+            targetName: "フォーメーション",
+            save: { [unowned self] in
+                Realm.FormationTemplate.save(self.template)
+                //Realm.FormationTemplate.notifyChange()
+                self.dismiss()
+            },
+            dispose: { [unowned self] in
+                Image.delete(category: .formationTemplates, id: self.template.id)
+                self.dismiss()
+            }
+        )
+    }
+    
+    private func didMovedFormationItem(_ item: FormationTemplateItem, percentage: CGPercentage) {
+        Realm.FormationTemplate.write(template) { _ in
+            item.percentage = percentage
+        }
+//        Realm.FormationTemplate.write(template) {
+//            item.percentage = percentage
+//        }
+     }
 }
