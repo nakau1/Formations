a222980ad05308fa3fcbb47ec7d4f239c69aff8c
中安佑一 <yuichi.nakayasu@gmail.com>
2017-10-08 02:44:03 +0900

    フォーメーション雛形エンティティの構成修正

--- Formations/Sources/Modules/FormationTemplate/Models/FormationTemplateItem.swift
+++ Formations/Sources/Modules/FormationTemplate/Models/FormationTemplateItem.swift
@@ -1,9 +1,46 @@
-//
-//  FormationTemplateItem.swift
+// =============================================================================
 //  Formations
-//
-//  Created by 中安佑一 on 2017/10/07.
-//  Copyright © 2017年 yuichi.nakayasu@gmail.com. All rights reserved.
-//
-
+//  Copyright 2017 yuichi.nakayasu All rights reserved.
+// =============================================================================
 import Foundation
+import RealmSwift
+
+class FormationTemplateItem: RealmSwift.Object {
+    
+    var info = [String : Any?]()
+    
+    // MARK: - Properties
+    
+    /// ソート順
+    @objc dynamic var number = 1
+    
+    // パーセンテージ
+    @objc dynamic var xPercentage: Float = 0.0
+    @objc dynamic var yPercentage: Float = 0.0
+    
+    // ポジション
+    @objc dynamic var positionText: String = "MF"
+    
+    var percentage: CGPercentage {
+        get {
+            return CGPercentage(xPercentage.f, yPercentage.f)
+        }
+        set {
+            xPercentage = Float(newValue.x)
+            yPercentage = Float(newValue.y)
+        }
+    }
+    
+    var position: Position {
+        get {
+            return Position(rawValue: positionText)!
+        }
+        set {
+            positionText = newValue.rawValue
+        }
+    }
+    
+    override class func ignoredProperties() -> [String] {
+        return ["percentage", "position", "info"]
+    }
+}
--- Formations/Sources/Modules/FormationTemplate/Models/FormationTemplateModel.swift
+++ Formations/Sources/Modules/FormationTemplate/Models/FormationTemplateModel.swift
@@ -9,7 +9,35 @@ class FormationTemplateModel: RealmModel<FormationTemplate>, IdentifierGeneratab
     override func create() -> Entity {
         let ret = super.create()
         ret.id = generateIdentifier()
+        ret.items.append(objectsIn: createDefaultItems())
         return ret
+    }
+}
+
+// MARK: - FormationTemplateItem
+extension FormationTemplateModel {
+    
+    func createDefaultItems() -> [FormationTemplateItem] {
+        let data: [(percentage: CGPercentage, position: Position)] = [
+            (percentage: CGPercentage(0.500, 0.920), position: .goalKeeper),
+            (percentage: CGPercentage(0.678, 0.732), position: .defender),
+            (percentage: CGPercentage(0.318, 0.732), position: .defender),
+            (percentage: CGPercentage(0.934, 0.568), position: .defender),
+            (percentage: CGPercentage(0.069, 0.568), position: .defender),
+            (percentage: CGPercentage(0.500, 0.525), position: .midfielder),
+            (percentage: CGPercentage(0.773, 0.373), position: .midfielder),
+            (percentage: CGPercentage(0.246, 0.373), position: .midfielder),
+            (percentage: CGPercentage(0.500, 0.242), position: .midfielder),
+            (percentage: CGPercentage(0.632, 0.041), position: .forward),
+            (percentage: CGPercentage(0.358, 0.041), position: .forward),
+        ]
+        return data.enumerated().map { i, datum -> FormationTemplateItem in
+            let item = FormationTemplateItem()
+            item.percentage = datum.percentage
+            item.position   = datum.position
+            item.number     = i
+            return item
+        }
     }
 }
 
