38ee974480ade47cb4874ae6eef321d085f20957
中安佑一 <yuichi.nakayasu@gmail.com>
2017-10-09 19:47:50 +0900

    フォーメーション雛形のデフォルト値の順番を入れ替え(FWが先頭に来るように)

--- Formations/Sources/Modules/FormationTemplate/Models/FormationTemplateModel.swift
+++ Formations/Sources/Modules/FormationTemplate/Models/FormationTemplateModel.swift
@@ -19,17 +19,17 @@ extension FormationTemplateModel {
     
     func createDefaultItems() -> [FormationTemplateItem] {
         let data: [(percentage: CGPercentage, position: Position)] = [
-            (percentage: CGPercentage(0.500, 0.920), position: .goalKeeper),
-            (percentage: CGPercentage(0.678, 0.732), position: .defender),
-            (percentage: CGPercentage(0.318, 0.732), position: .defender),
-            (percentage: CGPercentage(0.934, 0.568), position: .defender),
-            (percentage: CGPercentage(0.069, 0.568), position: .defender),
-            (percentage: CGPercentage(0.500, 0.525), position: .midfielder),
-            (percentage: CGPercentage(0.773, 0.373), position: .midfielder),
-            (percentage: CGPercentage(0.246, 0.373), position: .midfielder),
-            (percentage: CGPercentage(0.500, 0.242), position: .midfielder),
-            (percentage: CGPercentage(0.632, 0.041), position: .forward),
             (percentage: CGPercentage(0.358, 0.041), position: .forward),
+            (percentage: CGPercentage(0.632, 0.041), position: .forward),
+            (percentage: CGPercentage(0.500, 0.242), position: .midfielder),
+            (percentage: CGPercentage(0.246, 0.373), position: .midfielder),
+            (percentage: CGPercentage(0.773, 0.373), position: .midfielder),
+            (percentage: CGPercentage(0.500, 0.525), position: .midfielder),
+            (percentage: CGPercentage(0.069, 0.568), position: .defender),
+            (percentage: CGPercentage(0.934, 0.568), position: .defender),
+            (percentage: CGPercentage(0.318, 0.732), position: .defender),
+            (percentage: CGPercentage(0.678, 0.732), position: .defender),
+            (percentage: CGPercentage(0.500, 0.920), position: .goalKeeper),
         ]
         return data.enumerated().map { i, datum -> FormationTemplateItem in
             let item = FormationTemplateItem()
