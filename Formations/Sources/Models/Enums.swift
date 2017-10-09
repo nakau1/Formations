924ed7bf41d475783541e20f2365ce2cc262e4aa
中安佑一 <yuichi.nakayasu@gmail.com>
2017-10-09 19:44:50 +0900

    フォーメーション用色にコメント追記

--- Formations/Sources/Models/Enums.swift
+++ Formations/Sources/Models/Enums.swift
@@ -13,10 +13,10 @@ enum Position: String {
     
     var backgroundColor: UIColor {
         switch self {
-        case .goalKeeper: return #colorLiteral(red: 0.8431372549, green: 0.6392156863, blue: 0.0862745098, alpha: 1)
-        case .defender:   return #colorLiteral(red: 0.1921568627, green: 0.4, blue: 0.8588235294, alpha: 1)
-        case .midfielder: return #colorLiteral(red: 0.3803921569, green: 0.6431372549, blue: 0.2823529412, alpha: 1)
-        case .forward:    return #colorLiteral(red: 0.6823529412, green: 0.0862745098, blue: 0.1921568627, alpha: 1)
+        case .goalKeeper: return #colorLiteral(red: 0.8431372549, green: 0.6392156863, blue: 0.0862745098, alpha: 1) // D7A316
+        case .defender:   return #colorLiteral(red: 0.1921568627, green: 0.4, blue: 0.8588235294, alpha: 1) // 3166DB
+        case .midfielder: return #colorLiteral(red: 0.3803921569, green: 0.6431372549, blue: 0.2823529412, alpha: 1) // 61A448
+        case .forward:    return #colorLiteral(red: 0.6823529412, green: 0.0862745098, blue: 0.1921568627, alpha: 1) // AE1631
         }
     }
     
